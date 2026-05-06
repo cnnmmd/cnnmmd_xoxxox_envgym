67ca64468f62db86eb0eb6f2329518882722497b
azsgit <10238943+azsgit@users.noreply.github.com>
2026-05-06 18:01:02 +0900

    add initial files

---
+++ manage/bin/params.sh
@@ -0,0 +1,4 @@
+pthdoc="${pthtop}"/export/cnf/xoxxox/docenv_envgym
+cnfimg="${pthtop}"/export/cnf/xoxxox/docenv_envgym.txt
+cnfcmp="${pthtop}"/export/cnf/xoxxox/docker_envgym.yml
+imgtgt='xoxxox/envgym:001'
